920a6387333c5d38b3bfc610640b487f628c3794
jieyingpeng <jipeng@syr.edu>
2016-01-16 13:17:59 -0800

    solved

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -19,7 +19,30 @@ The binary search algorithm begins by comparing the target value to the value of
  */
 
 var binarySearch = function (array, target) {
-	var half = array.length/2
+	
+  var sub = function (low, high) {
+    // If we are done, we haven't found the target.
+    if (high === low) { return null; }
+
+    // Find the middle point.
+    var mid = Math.floor((high - low) / 2) + low;
+
+    // See if we found the target.
+    if (array[mid] === target) {
+      return mid;
+
+    // If not, decide which half to recurse on.
+    } else if (array[mid] > target) {
+      return sub(low, mid);
+    } else {
+      return sub(mid, high);
+    }
+  };
+
+  // Do the recursion.
+ return sub(0, array.length);
+
+
 	
 
 };
